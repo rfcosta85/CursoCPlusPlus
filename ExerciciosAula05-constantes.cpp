46d9d77dcfe687bcf6c937d1f4b73f66f337bae2
rfcosta85 <rfcosta85@gmail.com>
2020-04-25 17:56:03 -0300

    Create ExerciciosAula05-constantes.cpp

---
+++ ExerciciosAula05-constantes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415 
+#define canal cout << "\nCanal do Chaos";
+
+int main (void)
+{
+	int vidas, tiros, life;
+	
+	vidas = 3;
+	tiros = 500;
+	life = 100;
+	
+	cout << pi;
+	canal;
+	
+	return 0;
+}
