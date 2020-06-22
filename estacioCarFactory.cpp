3f93acdccbd12f8be54fc73267a1403a3e54848b
rfcosta85 <rfcosta85@gmail.com>
2020-06-22 15:17:31 -0300

    Simulador de Preços
    O Programa tem como objetivo simular 4 modelos de um mesmo carro.
    O modelo precisou ser construindo usando apenas um do while e switch.

---
+++ estacioCarFactory.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <cstdlib>
+#include <locale>
+#define IPI 0.08
+#define PRICE 25000.00
+#define DISCOUNT 0.035
+#define INCREASEOFFACTORY 0.02
+
+using namespace std;
+
+int main(void)
+{
+	setlocale(LC_ALL,"");
+	
+	unsigned short model;
+	float price = 25000.00, discount = 0.035, increaseOfFactory = 0.02;
+	float priceWithIpi;
+	float finalPriceWithIncreaseOfFactory = (price * increaseOfFactory) + price;
+	float finalPriceWithIncreaseOfFactoryAndIpi = (priceWithIpi * increaseOfFactory) + priceWithIpi;
+	float finalPriceComplete = (priceWithIpi + (priceWithIpi * increaseOfFactory) + (price * increaseOfFactory * 2) ) * discount + priceWithIpi;
+	char answer = 'S';	
+	
+	do
+	{	
+		
+		system("cls");
+	
+		cout << "----------------------------------------\n";
+		cout << "Fábrica de Automóvel Estácio\n";
+		cout << "Estácio Car 2020 \n";
+		cout << "----------------------------------------\n";
+		cout << "O modelo conta com 4 opções de utilitários\n";
+		cout << "Modelos:\n";
+		cout << "------------------------------------------\n";
+		cout << "0- Básico\n";
+		cout << "1- Trio elétrico\n";
+		cout << "2- Ar condicionado\n";
+		cout << "3- Direção Hidráulica\n";
+		cout << "4- Completo\n";
+		cout << "------------------------------------------\n";
+		
+		cout << "Escolha a sua opção: ";
+		cin >> model;		
+		
+		switch(model)
+		{
+			case 0:
+			{
+				
+				cout << "Esse modelo não conta com adicionais como ar condicionado e vidros elétricos\n";
+				cout << "----------------------------------------------------------------------------\n";
+				cout << "Preço: R$: " << (PRICE * IPI) + PRICE;
+				
+				break;
+			} 
+			
+			case 1: 
+			{
+				cout << "Trio Elétrico.\n";
+				cout << "Esse modelo conta com alarme, vidro elétrico e tranca elétrica.\n";
+				cout << "Mais segurança e comodidade para o seu veículo.\n";
+				cout << "---------------------------------------------------------------\n";
+				
+				cout << "Preço: R$: " << ((PRICE * IPI) + PRICE) * INCREASEOFFACTORY + (PRICE * IPI) + PRICE; 
+				
+				break;
+			}	
+			
+			
+			case 2:
+			{
+				
+				cout << "Ar Condicionado. \n";
+				cout << "Esse modelo conta com ar condicionado.\n";
+				cout << "Dirija sem se preocupar com o calor externo.\n";	
+				cout << "-------------------------------------------\n";
+				
+				cout << "Preço: R$: " << (PRICE * INCREASEOFFACTORY) + PRICE;
+				break;
+				
+			}
+			
+			case 3:
+			{
+				
+				cout << "Direção Hidráulica.\n";
+				cout << "Este modelo conta com uma direção Hidráulica.\n";
+				cout << "Mais segurança e comodidade para o seu veículo.\n";
+				cout << "-----------------------------------------------\n";
+				
+				cout << "Preço: R$: " << (PRICE * INCREASEOFFACTORY) + PRICE;
+				break;
+			}
+			
+			case 4:
+			{
+				cout << "Completo.\n";
+				cout << "Este modelo conta com todos os adicionais disponíveis.\n";
+				cout << "Segurança, conforto e comodidade. tudo em um só pacote.\n";
+				cout << "-------------------------------------------------------\n";					
+				
+				cout << "Preço: R$: " << (((PRICE * IPI) + PRICE) + (((PRICE * IPI) + PRICE) * INCREASEOFFACTORY) + ((PRICE * INCREASEOFFACTORY) * 2)) * DISCOUNT + (PRICE * IPI) + PRICE;
+				break;
+			}						
+						
+		
+		}
+		
+		cout << "\nDeseja simular outro modelo (S/N)? ";
+		cin >> answer;
+		
+	}while(answer != 'N');
+	
+	
+	return 0;
+}
+
