32dc4f1e94497cb87d3b45fa91d6b4141bfac9aa
rfcosta85 <rfcosta85@gmail.com>
2020-07-27 20:12:04 -0300

    Aula 17 - Comando For

---
+++ Exercicios17-ComandoDeLoopFor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int x, y, z;
+	
+	for(x = 0, y = 1, z=0; x <= 10 && z <= 6; x++, y+=2, z+=2)
+	{
+		cout << x << "-" << y << "-" << z << "\n";		
+	}
+	
+	return 0;
+}
