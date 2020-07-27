2cf34036375606166adaaeb83e29f319441af3d6
rfcosta85 <rfcosta85@gmail.com>
2020-07-27 20:11:49 -0300

    Aula 016 - Do While

---
+++ ExerciciosAula16-ComandoDeLoopDoWhile.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	
+	int cont;
+	cont = 20;
+	
+	do
+	{
+		cout << "Canal Fessor Bruno - " << cont << "\n";
+		
+	}while(cont < 20);
+	
+	
+	return 0;
+}
