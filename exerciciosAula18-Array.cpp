9ed26008eba65f90bb913e5cfd5e15e537fe530d
Rodrigo Costa <57047212+rfcosta85@users.noreply.github.com>
2020-08-18 15:40:19 -0300

    Translante code and include jason doc
    C++ now using Vscode on linux

--- exerciciosAula18-Array.cpp
+++ exerciciosAula18-Array.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main(void)
 {
 	
-	int vetor[7] = {10,20,30,40,50,90,192};	
+	int array[7] = {10,20,30,40,50,90,192};	
 	
 	
-	for(int i = 0; i < sizeof(vetor)/4; i++)
+	for(int i = 0; i < sizeof(array)/4; i++)
 	{
-		cout << vetor[i] << "\n";	
+		cout << array[i] << "\n";	
 	}	
+
+	cout << "\n";
+
+	// We divided the sizeof by 4 because witout 4 he will return the length in bytes of the array, because array have 4 bytes.
 	
 	return 0;
 }
