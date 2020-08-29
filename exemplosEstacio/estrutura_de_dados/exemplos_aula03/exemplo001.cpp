8b9524bc152035d8b674f925de8b433fb0d79cc9
Rodrigo Costa <57047212+rfcosta85@users.noreply.github.com>
2020-08-29 11:26:38 -0300

    Aula 03 - Estrutura de dados - Exemplo 01
    Exemplo 01: Atribuindo valores aos membros da Struct.

---
+++ exemplosEstacio/estrutura_de_dados/exemplos_aula03/exemplo001.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+
+    struct coordenadas
+    {
+        
+        int x, y;
+
+    };
+
+    coordenadas a = {-5,2}, b = {6,5};
+
+    cout << "\nCoordenadas de a("<< a.x <<","<< a.y <<")";
+    cout << "\nCoordenadas de b("<< b.x <<","<< b.y <<")";
+    cout << "\n\n";
+
+    getchar;
+    getchar;    
+    
+
+}
